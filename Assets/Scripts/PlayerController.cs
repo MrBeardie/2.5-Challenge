228477c1fabf136ba4c5b674a59705f3ad3cc827
MrBeardie <47094143+MrBeardie@users.noreply.github.com>
2019-03-10 18:48:28 -0700

    Add files via upload

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+public class PlayerController : MonoBehaviour
+
+
+{
+    private Rigidbody2D rb2d;
+    private int count;
+    private int health;
+    public Text countText;
+    public Text healthText;
+    public Text winText;
+    public Text loseText;
+    public float speed;
+    public float jumpForce;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        count = 0;
+        health = 3;
+        winText.text = "";
+        SetCountText();
+        SetHealthText();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("escape"))
+            Application.Quit();
+    }
+
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        Vector2 movement = new Vector2(moveHorizontal, 0);
+        rb2d.AddForce(movement * speed);
+    }
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if(collision.collider.tag == "ground")
+        {
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                rb2d.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+            }
+        }
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        //Check the provided Collider2D parameter other to see if it is tagged "PickUp", if it is...
+        if (other.gameObject.CompareTag ("PickUp"))
+        {
+            other.gameObject.SetActive (false);
+            count = count + 1;
+            SetCountText();
+        }
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            other.gameObject.SetActive(false);
+            health = health - 1;
+            SetHealthText();
+        }
+    }
+    void SetCountText ()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >=4)
+        {
+            winText.text = "You win!";
+        }
+    }
+    void SetHealthText()
+    {
+        healthText.text = "Health: " + health.ToString();
+        if (health >= 0)
+        {
+            loseText.text = "You lose";
+        }
+    }
+}
+     
